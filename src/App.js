e9e36435146193b001164f61cb01b58719543b0f
Saksham <skshm.090898@gmail.com>
2024-05-16 17:42:08 +0530

    setuped the react bootstrap

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="primary">Primary</Button>
+    </div>
+  );
+}
+
+export default App;
